5f5d5399a77d10086e7b44392716b10c0ae90e41
Drew Showalter <maelstroswim@gmail.com>
2018-03-24 18:31:03 +1100

    :recycle: multi-process image manip, & export more data.

--- image_manip/generate_seed_image.js
+++ image_manip/generate_seed_image.js
@@ -1,19 +1,20 @@
-const jsonfile = require('jsonfile');
 const axios = require('axios');
 const jimp = require('jimp');
 const fs = require('fs');
 
 const TYPES = require('./types');
+const POKE_URL = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full';
 
+const duoHexStringify = (stat) => Math.round(Number.parseInt(stat, 10)).toString(16).padStart(2, 0);
 
-const saveImage = async (url, pokeNumber) =>
+const saveImage = async (url, pokeNumber, type, dimension) =>
   jimp.read(url).then(image => {
-    const resizedBlackBackground = image.autocrop().resize(96, 96).opaque();
-    resizedBlackBackground.write(`seed_data/color_${pokeNumber}.png`);
-    return resizedBlackBackground.dither565().greyscale().write(`seed_data/grey_${pokeNumber}.png`);
+    const resizedBlackBackground = image.background(0xFFFFFFFF).rgba(false).resize(dimension, dimension);
+    resizedBlackBackground.write(`seed_data/${type}/color/${pokeNumber}.png`);
+    return resizedBlackBackground.dither565().greyscale().write(`seed_data/${type}/grey/${pokeNumber}.png`);
   });
 
-/*
+/**
 * function that writes a json file to disk describing a pokemon
 * @param {String} id-number
 * @return {undefined|Error}
@@ -29,36 +30,38 @@ module.exports.getPokemon = async pokeNumber => {
     }
   } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokeNumber}`);
 
-  /*
+  /**
   * we intend to leave the types in whatever order they're supposed to come in, just in case.
   * it seems likely that the first type is the more dominantly expressed design choice
   * There are 394 (18 * 18) possibilities, so sadly not evenly distributed in base 16
   * monotypes are expressed as a doublet
   */
-  const typeName1 = types.find(type => type.slot === 1).type.name
+  const typeName1 = types.find(type => type.slot === 1).type.name;
   const type1 = `${TYPES[typeName1]}`;
   const typeSlot2 = types.find(type => type.slot === 2);
   const type2 = typeSlot2 ? `${TYPES[typeSlot2.type.name]}` : type1;
 
-  /*
+
+  /**
   * returns a big string that's really a set of six numbers (0-255) representing the stats, in order
   * (speed, hp, attack, defense, special attack, special defense)
   * this is useful in determining what traits correspond to what stats
   * (think [bouba & kiki](https://www.psychologicalscience.org/news/releases/words-can-sound-round-or-sharp-without-us-realizing-it.html))
   */
-  const statString = ['speed','hp','attack','defense','special-attack','special-defense'].reduce((ret, statName) => {
-    const number = stats.find(({ stat, base_stat }) => stat.name === statName);
-    const normalNumber = JSON.stringify(Math.max(Math.min(number.base_stat, 255), 0));
-    return `${ret}${normalNumber.padStart(3, 0)}`;
+ const statString = ['speed','hp','attack','defense','special-attack','special-defense'].reduce((ret, statName) => {
+   const number = stats.find(({ stat, base_stat }) => stat.name === statName);
+   const normalNumber = JSON.stringify(Math.max(Math.min(number.base_stat, 255), 0));
+   return `${ret}${duoHexStringify(normalNumber)}`;
   }, '');
+  const typeString = `${duoHexStringify(type1)}${duoHexStringify(type2)}`;
+  const heightString = duoHexStringify(height);
+  const weightString = weight.toString(16).padStart(4, 0);
 
-  const json = {
-    height: JSON.stringify(height),
-    weight: JSON.stringify(weight),
-    type: `${type1.padStart(3, 0)}${type2.padStart(3, 0)}`,
-    stats: statString
-  };
+  const metaBin = `${typeString}${statString}${heightString}${weightString}`;
 
-  jsonfile.writeFileSync(`seed_data/${pokeNumber}.json`, json, { spaces: 2 });
-  await saveImage(front_default, pokeNumber);
+  const paddedNumber = `${pokeNumber}`.padStart(3, 0);
+
+  fs.writeFileSync(`seed_data/bins/${paddedNumber}`, metaBin);
+  await saveImage(front_default, paddedNumber, 'icon', 96);
+  await saveImage(`${POKE_URL}/${paddedNumber.padStart(3, 0)}.png`, paddedNumber, 'art', 475);
 };
--- image_manip/generate_seed_images.js
+++ image_manip/generate_seed_images.js
@@ -1,37 +1,32 @@
 const fs = require('fs');
 const axios = require('axios');
-const { getPokemon } = require('./generate_seed_image');
+const { spawn } = require('child-process-promise');
 
-/*
-* Function that starts grabbing and processing all pokemon on a single page response
-* since pokeapi/pokemon returns paginated numbers of pokemon to grab, 20 at a time
-* @param {String} url
-* return {String|null} next-page url
-*/
-const batch = async (url) => {
-  const  { data: { count, results, next } } = await axios.get(url);
-  console.log('next', next)
+const MAX_INDEX = 720;
 
-  await Promise.all(results.map(singlePoke => {
-    const pokeNum = singlePoke.url.replace(/\/$/, '').match(/\d+$/ig)[0];
-    return getPokemon(Number.parseInt(pokeNum, 10));
-  }));
-  console.log(`${100 * Number.parseInt(url.match(/\d+$/) ? url.match(/\d+$/)[0] : 0, 10) / count}% done!`);
-  return next;
-};
-
-
-const startBatching = async (url) => {
+const startMultiBatching = async (size) => {
   try {
     fs.mkdirSync('seed_data');
+    fs.mkdirSync('seed_data/bins');
   } catch (e) {
     console.log("Error creating seed_data folder. Continuing, assuming it's there already")
   }
 
-  let nextToDo = await batch(url);
-  while (nextToDo) {
-    nextToDo = await batch(nextToDo);
+  let startIndex = 0;
+  while (startIndex <= MAX_INDEX) {
+    console.log('startIndex', startIndex)
+    const nextTodo = [...Array(size).keys()];
+    try {
+      await Promise.all(
+        nextTodo.map((i) =>
+          spawn('bin/batch_worker', [`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${startIndex + (20 * i)}`], { stdio: 'inherit' })
+        )
+      );
+      startIndex += 80;
+    } catch (e) {
+      console.log('RREEEeeeeeeeeee', e);
+    }
   }
-}
+};
 
-startBatching('http://pokeapi.co/api/v2/pokemon');
+startMultiBatching(4);
