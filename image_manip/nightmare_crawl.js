35e1f6130940cce62a322549b59c14c0701bd6e5
Drew Showalter <maelstroswim@gmail.com>
2018-03-30 14:17:59 +1100

    :recycle: patch nightmare m/f failure.

--- image_manip/nightmare_crawl.js
+++ image_manip/nightmare_crawl.js
@@ -1,17 +1,29 @@
 const Nightmare = require('nightmare');
 
 module.exports.getAddress = async (pokeNumber) => {
-  const nightmare = new Nightmare();
+  const nightmare = new Nightmare({ waitTimeout: 8000 });
   let returnImageSrc = '';
-
+  
   try {
     returnImageSrc = await nightmare
       .goto(`https://bulbapedia.bulbagarden.net/wiki/File:Spr_7s_${pokeNumber}.png`)
-      .wait('.fullImageLink')
+      .wait('#filelinks')
       .evaluate(() => ([...document.querySelectorAll('.fullImageLink img')][0].currentSrc))
-      .end();
+      .end()
+      .then(imageUrl => imageUrl)
+      .catch(async () => {
+        const secondEvent = new Nightmare({ waitTimeout: 8000 });
+        const gender = ['m', 'f'][Math.round(Math.random())];
+
+        returnImageSrc = await secondEvent
+          .goto(`https://bulbapedia.bulbagarden.net/wiki/File:Spr_7s_${pokeNumber}_${gender}.png`)
+          .wait('#filelinks')
+          .evaluate(() => ([...document.querySelectorAll('.fullImageLink img')][0].currentSrc))
+          .end();
+        return returnImageSrc;
+    });
   } catch (e) {
-    console.error(`Nightmare error at pokemon ${pokeNumber}`);
+    console.error(`Nightmare error at pokemon ${pokeNumber}`, e);
   }
 
   return returnImageSrc;
